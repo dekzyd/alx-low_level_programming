183d827840b8e16073376418a4effa03afc29044
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-08 16:27:35 -0700

    Pass task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	fwrite("with proper grammar, but the outcome is a piece of art,\n", 56, 1, stderr);
+	return (0);
+}
