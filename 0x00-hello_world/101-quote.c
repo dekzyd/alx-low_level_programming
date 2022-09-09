650e75c16873340ad8947861a7bc41b9ad73ae82
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-08 17:17:29 -0700

    7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,8 @@
 */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
-return (1);
+
+char string[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	fwrite(string, 59, 1, stderr);
+	return (1);
 }
