ae52f4d88a3d4de5bd15da43234d6dbcbb6c3748
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-09 17:04:53 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; ++c)
+	{
+		putchar(c);
+		putchar(',');
+		putchar(' ');
+	};
+	putchar('\n');
+
+	return (0);
+}
