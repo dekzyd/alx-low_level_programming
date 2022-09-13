6691480f5fe48065766e9759ef34b546ddf8f449
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-13 16:58:18 -0700

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,12 @@ int print_last_digit(int n)
 {
 	int last;
 
-	last = n % 10;
-	return _abs(last);
+	last = (n % 10);
+	
+	if (last < 0)
+	{
+		last = (last * -1);
+	}
+	_putchar(last + '0');
+	return last;
 }
