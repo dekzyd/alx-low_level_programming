946a065c272197417d8945573fcc943fb9ead8d7
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-13 12:53:45 -0700

    pass 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks if argument isin lowercase
+ *
+ * Return: Success (1) Failure(0).
+ */
+int _islower(int c)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c) 
+		{
+			return (1);
+		};
+	};
+	return (0);
+}
