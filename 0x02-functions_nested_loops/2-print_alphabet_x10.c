c98a3d4da39d2a98e71e10f4bdf3790abeebf69f
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-13 11:11:25 -0700

    pass 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets in lowercase 10 times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int j;
+	j = 0;
+
+	while (j < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		j++;
+	}
+
+}
