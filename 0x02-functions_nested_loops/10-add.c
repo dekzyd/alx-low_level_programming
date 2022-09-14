ee4a6a7ad9b714cfb50a171c17830d2c89e1fa66
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-13 17:15:43 -0700

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - adds two integers
- *@n1, n2: The numbers to be added
- *
+ *@n: The numbers to be added
+ *@k: The number to be added.
  * Return: sum of ni and n2 of n.
  */
 int add(int n, int k)
