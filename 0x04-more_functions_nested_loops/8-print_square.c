26c902298d969171f58cb20e47384d8ee018773c
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-15 14:24:58 -0700

    create 8-print_square.c and add ptototype to main.h

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_square - prints squares
+ * @size: the amount of chars
+ *
+ * Return: returns void
+ */
+print_square(int size)
+{
+	int c,j;
+
+	if (s <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (j = 0; j < size; j++)
+		{
+			for (c = 0; c < size; c++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
