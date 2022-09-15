96e8bb3152b5af0718a82bcb0651b1557ab12a89
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-15 16:18:41 -0700

    fix putchar bug in 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,17 @@
  */
 void more_numbers(void)
 {
-	int c, j;
+	int c;
+	int j;
 
 	for (j =  0; j < 10; j++)
 	{
 		for (c = 0; c <= 14; c++)
 		{
 			if (c >= 10)
-			putchar((c / 10) + '0');
-			putchar((c % 10) + '0');
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
