12af5e4bd60907a6bbefe5b4d4534c1a2b692f1d
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-15 18:13:53 -0700

    create 7-print_diagonal.c and include prototype to main.h

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print-diagonal - draw diagonal 
+ * @n: the number of times
+ * Return: Always 0
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			int j;
+
+			for (j = 0; i <= i; j++)
+			{
+				if (j == 1)
+					_putchar('\\');
+				else
+					_putchar(' ');
+
+			}
+			_putchar('\n');
+		}
+	}
+}
