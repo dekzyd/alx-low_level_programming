12366119736990f9561b4e9781af79124befc65e
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-19 04:26:06 -0700

    create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ * @s: the string to be read
+ *
+ * Return: success 0
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		_putchar(*(s + i));
+	}
+
+	return (0);
+}
