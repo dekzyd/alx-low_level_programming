5d17c47e61d0938afa04aa3b6e84e1d11e8656db
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-19 09:39:57 -0700

    create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @str: the string to be reversed
+ *
+ * Return: void
+ */
+void rev_string(char *str)
+{
+	int i, temp;
+	int j, ln;
+	/* calc length of string */
+	ln = 0;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		ln++;
+	}
+
+	/* reverse string */
+	for (j = 0; j < ln/2; j++)
+	{
+		temp = *(str + j);
+		*(str + j) = *(str + ln - j -1);
+		*(str + ln -j -1) = temp;
+
+	}
+
+}
