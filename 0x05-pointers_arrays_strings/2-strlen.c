36961f279eb975eb913d2a5d41c4303054612a4b
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-19 03:15:31 -0700

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.io>
+
+/**
+ * _strlen - returns length of a string
+ * @s: the strings length to be read
+ *
+ * Return: success length of string
+ */
+int _strlen(char *s)
+{
+	/* l = 1 to cover up for '\0' char in string*/
+	int l = 1;
+
+	for (int i = 0; *(s + i) != '\0'; i++)
+	{
+		l++;
+	}
+
+	return (l);
+}
