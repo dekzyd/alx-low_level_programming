7e11c967ce569f6e0643c0a587ba84c32c12c01f
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-19 16:12:00 -0700

    create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints n number of array items.
+ * @n: Number of array items to print
+ * @a: The array to be read
+ *
+ * return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d\n", *(a + i));
+		else
+			printf("%d, ", *(a + i));
+	}
+
+}
