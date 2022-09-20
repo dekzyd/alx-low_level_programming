b09a1ac3d5aa5dcfcd4a12a04ba51b4688bd18ee
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-19 19:40:29 -0700

    create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - copies the str from src to dest.
+ * @src: string to be copied
+ * @dest: copied str
+ *
+ * Return: returns char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, lent;
+	
+	lent = strlen(src);
+	for (i = 0; i <= lent; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+return (dest);
+}
