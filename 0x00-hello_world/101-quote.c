dbe702043b9a3996d0012627eb2bddd3a576423a
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-24 17:26:11 -0700

    Change header to unistd.io in 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
 * main - Entry point
@@ -9,6 +9,6 @@ int main(void)
 {
 
 	char string[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, string, 59);
+	write(1, string, sizeof(string));
 	return (1);
 }
