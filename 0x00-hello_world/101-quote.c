66cbf6a3c75029b4036eaf1c400ce1e67d38562b
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-24 17:32:44 -0700

    Change sizeof to 59 in 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
 int main(void)
 {
 
-	char string[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, string, sizeof(string));
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, str, 59);
 	return (1);
 }
