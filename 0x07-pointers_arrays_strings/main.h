dbf2c9e1499f06f5306d4230cf1cf5dd7cb00191
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-26 21:26:31 -0700

    create 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
 unsigned int _strspn(char *s, char *accept);
 int _isupper(int c);
 int _isdigit(int c);
