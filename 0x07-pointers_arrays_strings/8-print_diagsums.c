94d96ec9304ccdba5908d53e3b60843b07ac7b0c
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-26 21:56:03 -0700

    create 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,11 +3,27 @@
 
 /**
  * print_diagsums - prints diagonals
- * @a: 2d array 
+ * @a: 2d array
+ * @size: size of array
  *
  * Return: void
  */
 void print_diagsums(int *a, int size)
 {
-	
+	int prim_d = 0;
+	int sec_d = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		prim_d += a[i];
+		a += size;
+	}
+	a -= size;
+	for (i = 0; i < size; i++)
+	{
+		sec_d += a[i];
+		a -= size;
+	}
+	printf("%d, %d\n", prim_d, sec_d);
 }
