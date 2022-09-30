52671965c3d0ff1e40905e1b437e53dc2c8e6c09
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-30 16:59:35 -0700

    create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of args passed into it.
+ * @argc: argument count
+ * @argv: argument vector that contains strings of args
+ *
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
