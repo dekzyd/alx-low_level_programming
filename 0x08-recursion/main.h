ac779cd43e92e11b45a56bac07e1df7bc0e540fb
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-29 18:05:31 -0700

    create 6-is_prime_number.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
