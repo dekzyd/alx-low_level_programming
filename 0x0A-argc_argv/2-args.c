bc34420bf2db2397f3099b745eba30e9f0e3e4d5
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-09-30 17:28:35 -0700

    create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all args
+ * @argc: argument count
+ * @argv: argument vector with name of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}
