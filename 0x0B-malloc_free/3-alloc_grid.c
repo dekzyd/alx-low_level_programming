1707cf313b931c758ad1d38a22e59ff237cdeb29
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-10-04 14:11:49 -0700

    Create 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a 2d array
+ * @width: width of the array
+ * @height: height of the array
+ *
+ * Return: pointer to array.
+ */
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	int **ptr, i, j;
+	/*allocate mem to ptr*/
+	ptr = malloc(height * sizeof(int *));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	/*allocate mem for ptr[i] i.e rows*/
+	for (i = 0; i < width; i++)
+	{
+		ptr[i] = malloc(width * sizeof(int));
+	}
+	/* fill values */
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+
+}
