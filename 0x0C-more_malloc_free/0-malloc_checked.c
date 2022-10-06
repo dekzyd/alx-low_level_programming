7e01d2ca063523d73a0c1b113dcbcf15ace55642
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-10-06 02:55:56 -0700

    Create 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib>
+
+/**
+ * malloc_checked - allocates memory
+ * @b: size in bytes
+ *
+ * Return: pointer to allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
