f59a626a7798476806db516163142d144add5de6
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-10-06 16:51:52 -0700

    fix betty errors

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,8 +15,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-		return(NULL);
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
