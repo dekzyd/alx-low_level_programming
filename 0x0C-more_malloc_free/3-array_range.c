0cd97c68fb25c122cc5764a3f696eb6c452dbc4b
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-10-06 18:14:29 -0700

    Fix malloc declaration

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	ptr = malloc(max * sizeof(int));
+	ptr = malloc((max - min + 1) * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; min <= max; i++)
