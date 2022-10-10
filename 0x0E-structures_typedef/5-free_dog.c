aa27555b47252dce6bf3e72ee2c8a0d2ba510a41
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-10-10 04:43:07 -0700

    create 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for dogs
+ * @d: pointer to dog to be freed
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
