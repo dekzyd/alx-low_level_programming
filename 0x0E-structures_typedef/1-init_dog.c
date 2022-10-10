3954dd4108cf67b196d90d4cce68f74f9184793e
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-10-10 13:03:02 -0700

    update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,10 +11,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *ptr;
 
-	ptr = d;
-	(*ptr).name = name;
-	(*ptr).age = age;
-	ptr->owner = owner;
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		d->owner = owner;
+	};
 }
