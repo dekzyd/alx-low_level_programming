413423eeea2ce31460bc35389d9826dd96b6166e
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-10-10 00:57:01 -0700

    create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structs
+ * @name: dog name
+ * @age: dog age
+ * @owner: name of owner
+ *
+ * Description: struct for type of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
