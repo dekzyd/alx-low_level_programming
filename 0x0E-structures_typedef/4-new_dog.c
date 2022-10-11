03765cfc6c326776e8963e2386ec10532cd98d37
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-10-10 19:05:59 -0700

    create 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,55 +1,67 @@
 #include "dog.h"
-#include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * new_dog - creates a new dog object from existing struct.
- * @name: dog name
- * @age: dog age
- * @owner: dog owner
+ * _strlen - gets len of str
+ * @s: string to get length of string
  *
- * Return: pointer to new dog.
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int m = 0;
+
+	for (m = 0; s[m]; m++)
+		;
+	return (m);
+}
+
+/**
+ * _strcpy - copies string from b to a
+ *
+ * @a: pointer to destination of string
+ * @b: pointer to source string to copy from dest
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *a, char *b)
+{
+	char *x = a;
+
+	while (*b)
+		*x++ = *b++;
+	*x = '\0';
+	return (a);
+}
+
+/**
+ * new_dog - makes a new dog, memory and all
+ * @name: name to create mem and assign to for dog
+ * @age: age to assign to for dog
+ * @owner: owner to create mem and assign to for dog
+ *
+ * Return: pointer to new dog_t (struct dog)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
-	unsigned int nl, ol, i;
+	dog_t *new_d;
 
-	nl = strlen(name);
-	ol = strlen(owner);
-	if (name == NULL || owner == NULL)
-	{
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
 		return (NULL);
-	}
-	ptr = malloc(sizeof(dog_t));
-	if (ptr == NULL)
+	if (name == NULL)
+		new_d->name = NULL;
+	else
 	{
-		return (NULL);
+		new_d->owner = malloc(_strlen(owner) + 1);
+		if (new_d->owner == NULL)
+		{
+			free(new_d->name);
+			free(new_d);
+			return (NULL);
+		}
+		new_d->owner = _strcpy(new_d->owner, owner);
 	}
-
-	ptr->name = malloc(sizeof(char) * nl);
-	if (ptr->name == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	for (i = 0; i < nl; i++)
-	{
-		ptr->name[i] = name[i];
-	}
-	ptr->age = age;
-	ptr->owner = malloc(sizeof(char) * ol);
-	if (ptr->owner == NULL)
-	{
-		free(ptr->name);
-		free(ptr);
-		return (NULL);
-	}
-	for (i = 0; i < ol; i++)
-	{
-		ptr->owner[i] = owner[i];
-	}
-
-	return (ptr);
+	new_d->age = age;
+	return (new_d);
 }
