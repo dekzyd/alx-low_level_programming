5a2f956bfea6731f6a22e86b1a8c8a1055eace03
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-10-12 14:05:58 -0700

    Create 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an int
+ * @size: number of elements in the array
+ * @cmp: pointer to the function to be used to compare values
+ * @array: array of ints to be compared
+ *
+ * Return: index of the first element when cmp function
+ * does not return 0, -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			if ((cmp)(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
