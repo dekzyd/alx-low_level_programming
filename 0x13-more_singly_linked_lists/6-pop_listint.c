9f2411c17e74593f6ae02d71fcc8330580e83c6c
Oladayo Adekoya <adekoyadayo2010@gmail.com>
2022-10-25 15:53:40 -0700

    Create 6-pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - deletes the head node of a listint_t list
+ * @head: pointer to the first node.
+ *
+ * Return: data of removed node.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int data;
+
+	if (head == NULL)
+		return (0);
+	ptr = (*head)->next;
+	data = (*head)->n;
+	free(*head);
+	(*head) = ptr;
+	return (data);
+
+}
